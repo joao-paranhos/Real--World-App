892268eee80ece14b97b3f18d1224f7768bf25b0
João Aurélio da Silva Paranhos <joaoaurelio091@gmail.com>
2024-08-07 19:39:36 -0300

    Adicionando fixtures

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -1,15 +1,16 @@
+import usuario from '../fixtures/Usuarios/Login.json' 
 const Chance = require ('chance')
 const chance = new Chance()
 
 describe('Iremos testar todos os cenarios atuais de login',()=>{
 
 beforeEach(() => {
-    cy.visit('http://localhost:3000/')
+    cy.visit('http://localhost:3000/signin')
 });
     it('Nesse cenário iremos logar  como um usuário de forma correta e validar a cor do botão de login',()=> {
 
-        cy.get('#username').type('Heath93')
-        cy.get('#password').type('s3cret')
+        cy.get('#username').type(usuario.Login_sucesso.Usuario)
+        cy.get('#password').type(usuario.Login_sucesso.Senha)
         cy.get('button').should('have.css','color','rgb(255, 255, 255)')
         cy.get('button').should('have.css','background-color','rgb(63, 81, 181)')
         cy.get('button').click()
@@ -29,8 +30,8 @@ beforeEach(() => {
 
     it('Tentativa de login com usuário válido e senha inválida mas que esteja dentro do nosso padrão com 4 ou mais caracteres',()=>{
 
-        cy.get('#username').type('Heath93')
-        cy.get('#password').type(1234)
+        cy.get('#username').type(usuario.Login_sucesso.Usuario)
+        cy.get('#password').type(usuario.Login_incorreto.Senha)
         cy.get('button').should('have.css','color','rgb(255, 255, 255)')
         cy.get('button').should('have.css','background-color','rgb(63, 81, 181)')
         cy.get('button').click()
@@ -40,20 +41,20 @@ beforeEach(() => {
     
     it('Nesse cenário iremos colocar um usuário válido, usando uma senha inválido com menos de 4 caracteres para verificar se o nosso botão ficará na cor cinza e desabilitado',()=>{
 
-        cy.get('#username').type('Heath93')
-        cy.get('#password').type(123)
+        cy.get('#username').type(usuario.Login_sucesso.Usuario)
+        cy.get('#password').type(usuario.Login_incorreto.Senha_3_caracteres)
         cy.get('button').should('have.css', 'color', 'rgba(0, 0, 0, 0.26)')
         cy.get('button').should('be.disabled')})
 
     it('Iremos testar se ao preencher somente a senha, para verificar se nosso botão fica cinza e desabilitado',() =>{
         
-        cy.get('#password').type('s3cret')
+        cy.get('#password').type(usuario.Login_sucesso.Senha)
         cy.get('button').should('have.css', 'color', 'rgba(0, 0, 0, 0.26)')
         cy.get('button').should('be.disabled')})
     
     it('Iremos testar se ao preencher somente o campo de usuario, para verificar se o nosso botão fica cinza e desabilitado',() =>{
         
-        cy.get('#username').type('Heath93')
+        cy.get('#username').type(usuario.Login_sucesso.Usuario)
         cy.get('button').should('have.css', 'color', 'rgba(0, 0, 0, 0.26)')
         cy.get('button').should('be.disabled')})
 
